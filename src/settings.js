5346bd377a87472a2c79dd27e2e16756b82fc205
Caesar454905970 <454905970@qq.com>
2022-03-30 09:48:55 +0800

    测试写死的路由，添加

--- src/settings.js
+++ src/settings.js
@@ -2,11 +2,11 @@
  * 全局环境设置
  */
 export default {
-  env:"test",
-  baseURL: "http://localhost:8081", //测试环境
+/*  env:"test",
+  baseURL: "http://localhost:8081", //测试环境*/
 
-/*  env:"product",
-  baseURL: "http://111.229.91.20:8081", //生产环境*/
+  env:"product",
+  baseURL: "http://111.229.91.20:8081", //生产环境
 
 
 
